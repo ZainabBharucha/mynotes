fa0679e889194ca6bd0f76bf05736e9f542a9b10
Zainab Bharucha <zainab.bharucha@gojo.co>
2023-04-09 22:34:55 +0530

    Added HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
+
 import 'firebase_options.dart';
 
 // Future main() async {
@@ -15,42 +16,16 @@ void main() {
       theme: ThemeData(
  
         
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
     ),);
 }
 
-class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
 
-  @override
-  State<RegisterView> createState() => _RegisterViewState();
-}
-
-class _RegisterViewState extends State<RegisterView> {
-
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 @override
-  void initState() {
-    // TODO: implement initState
-
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
-
- @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -65,51 +40,16 @@ class _RegisterViewState extends State<RegisterView> {
         builder: (context, snapshot) {
           switch(snapshot.connectionState){
             case ConnectionState.done:
-              return Column(
-          children: [
-            TextField(
-              controller: _email,
-              enableSuggestions: false,
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                hintText: "Enter your email here:"
-              ),
-            ),
-            TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: const InputDecoration(
-                hintText: "Enter your password:"
-              ),
-              
-            ),
-          
-          TextButton(
-            onPressed: () async {
-      
-            //  await Firebase.initializeApp(
-            //      options: DefaultFirebaseOptions.currentPlatform,
-            //   );
-            try{
-                final email = _email.text;
-              final password = _password.text;
-              final UserCredential = 
-                await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
-                print(UserCredential);
-            } on FirebaseAuthException catch(e){
-              print("Something bad happened");
-            }
-            },
-            child: const Text("Register")
-          )
-          ],
-        );
-
-        default:
-          return const Text("Loading...");
+               final user = FirebaseAuth.instance.currentUser;
+               if(user?.emailVerified??false){
+                print("Verified user");
+               }
+               else{
+                print("Unverified user");
+               }
+              return const Text("Done");
+            default:
+              return const Text("Loading...");
           }
 
           
@@ -118,6 +58,4 @@ class _RegisterViewState extends State<RegisterView> {
       ),  
     );
 }
-
 }
-
---
+++ lib/views/register_view.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../firebase_options.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+@override
+  void initState() {
+    // TODO: implement initState
+
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+ 
+
+}
+
