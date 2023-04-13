53b0c43059db9e923d4fc9271c6a6fd972e80014
Zainab Bharucha <zainab.bharucha@gojo.co>
2023-04-13 23:39:14 +0530

    Cleaned hardcoded routes

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 import '../firebase_options.dart';
 import 'dart:developer' as devtools show log;
 
@@ -60,7 +61,7 @@ class _LoginViewState extends State<LoginView> {
                 password: password,
               );
               Navigator.of(context).pushNamedAndRemoveUntil(
-                '/notes/',
+                notesRoute,
                 (route) => false,
               );
             } on FirebaseAuthException catch (e) {
@@ -76,7 +77,7 @@ class _LoginViewState extends State<LoginView> {
         TextButton(
             onPressed: () {
               Navigator.of(context)
-                  .pushNamedAndRemoveUntil('/register/', (route) => false);
+                  .pushNamedAndRemoveUntil(registerRoute, (route) => false);
             },
             child: const Text("Not registered yet? Register here!")),
       ]),
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'dart:developer' as devtools show log;
 
@@ -72,7 +73,7 @@ class _RegisterViewState extends State<RegisterView> {
         TextButton(
         onPressed: () {
           Navigator.of(context).pushNamedAndRemoveUntil(
-            '/login/',
+            loginRoute,
             (route) => false);
         },
         child: const Text("Login here!"))
